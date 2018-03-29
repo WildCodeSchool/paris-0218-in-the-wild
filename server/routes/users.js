52ead0ab57ab9e00c4df93260cd419660b9c259c
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-03-29 23:51:59 +0200

    adding new routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,25 +1,46 @@
-// Utilisation de Express
-const express = require ('express');
+// Require du fichier de config
+const router = require('./config')
 
-// Utilisation du routeur Express
-const router = express.Router();
-
-// Indique les headers à utiliser pour nos en-têtes
-router.use((req, res, next) => {
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-  })
-
-
-// Homepage du site pour charger tous les meetups en cours
-router.get( "/",(req,res,next) => {
+// Afficher tous les users inscrits
+router.get( "/users",(req,res,next) => {
 
     res.json(
         {
-            "message" : "je suis une eéponse"
+            "message" : "je suis une réponse"
         }   
     )
 })
-  
+
+// Afficher un user
+router.get("/users/:userId",(req,res,next)=>{
+    
+    let user = req.params.userId
+
+    res.send(`id user est ${user}`) // renvoi la réponse
+})
+ 
+// Insérer un nouvel user.
+router.post("/users/sign-up",(req,res,next)=>{
+    
+    // Objet json test
+    let user = {
+
+        "firstname" : "Sammy",
+        "lastname" : "kacemi",
+        "mail" : "sammy@wcs.com"
+    }
+
+    // Test de la réponse
+    res.json(`Bienvenue ${user.firstname} à In The Wild`)
+
+})
+
+// Modifier un user
+router.put("/users/update",(req,res,next)=>{
+
+    
+})
+
+module.exports = router
+
 
