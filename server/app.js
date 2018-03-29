94b5a59134eb61bd36a8f219772e359f4b259234
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-03-29 22:31:06 +0200

    create file app.js

---
+++ server/app.js
@@ -0,0 +1,11 @@
+// Utilisation de Express
+const express = require ('express');
+const app = express();
+
+
+// Définition de nos routes 
+const usersRoutes = require('./routes/users')
+
+
+
+module.exports = app;
