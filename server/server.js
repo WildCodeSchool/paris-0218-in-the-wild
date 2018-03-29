7d5a325a93b532f79a241646dff607fea0d16c99
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-03-29 22:29:56 +0200

    Create file server.js

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// Utilisation du package http-server
+const http = require ('http');
+// Utilisatio de express depuis le fichier app.js
+const app = require ('./app');
+
+// Déffinition du serveur
+const port = 3000;
+
+// Définition du serveur
+const server = http.createServer(app)
+
+// Lancer le serveur
+server.listen(port,()=>console.log(`Let's work on est connecté sur le port ${port}`))
+
+
+
+
+
