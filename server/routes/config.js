84a2de9163b4225f3eb4c821a7848b6328c3f9c3
WalterWhiteGit <walterwhitegithub@gmail.com>
2018-03-29 23:50:03 +0200

    creating config file

---
+++ server/routes/config.js
@@ -0,0 +1,17 @@
+// Utilisation de Express
+const express = require ('express');
+
+// Utilisation du routeur Express
+const router = express.Router();
+
+// Indique les headers à utiliser pour nos en-têtes
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+
+// Exportation du module pour une utilisation dans nos routes
+module.exports = router
+
+ 
