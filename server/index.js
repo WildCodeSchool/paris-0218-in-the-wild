7d71b72c34abab366219b336200944ad64be5686
Charlotte Guerin <fcgb_2611@hotmail.com>
2018-04-03 15:42:54 +0200

    Switch to express app instead of raw http

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((request, response) => {
-	console.log("j'ai une requête")
-	response.end("ok")
-	})
-server.listen(3247, () => console.log("j'écoute sur le port 3247"))
+// const server = http.createServer((request, response) => {
+// 	console.log("j'ai une requête")
+// 	response.end("ok")
+// 	})
+// server.listen(3247, () => console.log("j'écoute sur le port 3247"))
+
+const app = express()
+
+app.listen(3247, () => console.log("j'écoute sur le port 3247"))
+
+app.get('/', (request, response) => {
+	response.send("Ok")
+})
