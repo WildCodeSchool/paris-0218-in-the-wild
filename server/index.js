ca70757846bea3e8d1f980ce7d9bca4bb493f89c
Christopher Fremond <christopher.fremond@gmail.com>
2018-04-03 16:41:15 +0200

    index.js avec les eventsmocks

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
-const event1 = require('../events/1.son')
-console.log(event1)
+const event1 = require('../mocks/events/1.json')
+const event2 = require('../mocks/events/2.json')
+
+const events = [ event1, event2]
+
+console.log(event1.id)
 
 // const server = http.createServer((request, response) => {
 // 	console.log("j'ai une requête")
@@ -11,7 +15,7 @@ console.log(event1)
 
 const app = express()
 
-app.listen(3247, () => console.log("j'écoute sur le port 3247"))
+app.listen(3248, () => console.log("j'écoute sur le port 3247"))
 
 app.get('/', (request, response) => {
 	response.send("Ok")
