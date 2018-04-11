1175af30cd99efe7f2e630bae23de494664e4740
samuel <samlambert221@gmail.com>
2018-04-11 18:12:20 +0200

    event list page rendue dynamique

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,17 @@
+const createNewEvent = (boireEvent) =>
+  `<div>
+  <div class= "column event">
+  <p class="date">${boireEvent.startingTime}</p>
+  <h2 class="title">${boireEvent.title}</h2>
+  </div>
+
+  </div>
+  `
 /* global fetch */
-fetch('http://localhost:3248/events')
+fetch('http://localhost:3248/events/Boire')
   .then(res => res.json())
+  .then(boireEvents => {
+    const eventElement = document.getElementById('event')
+    const eventElements = boireEvents.map(createNewEvent).join('')
+    eventElement.innerHTML = eventElements
+  })
