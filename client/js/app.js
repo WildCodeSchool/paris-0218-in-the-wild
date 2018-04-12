d7e01dfde6b9de07c830301b3b0904b831f9ef93
samuel <samlambert221@gmail.com>
2018-04-12 18:03:52 +0200

    creation route catégorie boire & module boire

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,5 @@
 import {createNewEvent} from './components/event.js'
+import {showCategory} from './components/showCategory.js'
 
 /* global fetch */
 fetch('http://localhost:3248/events/Boire')
@@ -7,4 +8,14 @@ fetch('http://localhost:3248/events/Boire')
     const eventElement = document.getElementById('event')
     const eventElements = boireEvents.map(createNewEvent).join('')
     eventElement.innerHTML = eventElements
+
+    
+  })
+
+fetch('http://localhost:3248/categories')
+  .then(res => res.json())
+  .then(categories => {
+  	const eventCategory = document.getElementById('category')
+    const eventCategories = showCategory(categories[0])
+    eventCategory.innerHTML = eventCategories
   })
