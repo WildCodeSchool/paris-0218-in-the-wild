5d016ce49fc79565960bcbc4b6bc684b4790159e
HediK <hedi0475@gmail.com>
2018-04-12 18:29:52 +0200

    ajout du components formulaire

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,15 @@
+import { init } from './eventProposition/eventForm.js'
+import {form} from './components/eventProposition.js'
+
 /* global fetch */
 fetch('http://localhost:3248/events')
   .then(res => res.json())
+
+// injection des components
+
+// form
+
+const formContainer = document.querySelector('.container')
+formContainer.innerHTML = form()
+
+init()
