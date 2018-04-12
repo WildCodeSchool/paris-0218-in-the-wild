2ed8637273a382379c2eaeba4cc60c6d8e0dae45
samuel <samlambert221@gmail.com>
2018-04-12 09:11:41 +0200

    component folder added

--- client/js/app.js
+++ client/js/app.js
@@ -1,12 +1,3 @@
-const createNewEvent = (boireEvent) =>
-  `<div>
-  <div class= "column event">
-  <p class="date">${boireEvent.startingTime}</p>
-  <h2 class="title">${boireEvent.title}</h2>
-  </div>
-
-  </div>
-  `
 /* global fetch */
 fetch('http://localhost:3248/events/Boire')
   .then(res => res.json())
