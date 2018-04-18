ae807a7c63b63c89c1cfce88b2bef2ad186b9031
benoit <rameaubenoit@hotmail.fr>
2018-04-18 15:58:58 +0200

    forumulaire post route

--- client/js/app.js
+++ client/js/app.js
@@ -2,6 +2,7 @@ import {createNewEvent} from './components/event.js'
 import {showCategory} from './components/show-category.js'
 import {createHeader} from './components/nav.js'
 
+
 const params = new URLSearchParams(window.location.search)
 
 const category = params.get('category')
@@ -34,3 +35,30 @@ fetch(`http://localhost:3248/events/category/${category}`)
 
 const headerEl = document.getElementById('header')
 headerEl.innerHTML = createHeader({})
+
+
+
+document.getElementById('regForm').addEventListener('submit', event=> {
+ event.preventDefault()
+  const firstNameUsers = document.getElementById('fname').value
+  const lastNameUsers = document.getElementById('lsname').value
+  const username = document.getElementById('Username').value
+  const mail = document.getElementById('mailing').value
+  const schoolist = document.getElementById('school').value
+  const password = document.getElementById('password').value
+  const confirmPassword = document.getElementById('yourpass').value
+ 
+fetch('http://localhost:3248/users', {
+ method: 'post',
+ body: JSON.stringify({
+  firstName: firstNameUsers,
+  lastName: lastNameUsers,
+  pseudo: username,
+  email: mail,
+  schoolName: schoolist,
+  password: password,
+  validePassword : confirmPassword,
+ })
+    }).then(res => console.log(res.status, res.statusCode))
+})
+
