3abc50560d757345532b226a864cef4f4213c543
Charlotte Guerin <fcgb_2611@hotmail.com>
2018-04-23 00:17:47 +0200

    Module navbar et sidebar

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 import {createNewEvent} from './components/event.js'
-import {showCategory} from './components/show-category.js'
-import {createHeader} from './components/nav.js'
+// import {showCategory} from './components/show-category.js'
+import {createHeader, createSidebar} from './components/nav.js'
 
 const params = new URLSearchParams(window.location.search)
 
@@ -12,25 +12,28 @@ if (!category) {
 
 // console.log({ category })
 
-fetch('http://localhost:3248/categories')
-  .then(res => res.json())
-  .then(categories => {
-    // categories.map(categoryLink).join('')
-    const eventCategory = document.getElementById('category')
-    const matchedCategory = categories
-      .find(cat => cat.normalizedTitle === category)
-    const eventCategories = showCategory(matchedCategory)
-    eventCategory.innerHTML = eventCategories
-  })
+// fetch('http://localhost:3248/categories')
+//   .then(res => res.json())
+//   .then(categories => {
+//     // categories.map(categoryLink).join('')
+//     const eventCategory = document.getElementById('category')
+//     const matchedCategory = categories
+//       .find(cat => cat.normalizedTitle === category)
+//     const eventCategories = showCategory(matchedCategory)
+//     eventCategory.innerHTML = eventCategories
+//   })
 
 /* global fetch */
-fetch(`http://localhost:3248/events/category/${category}`)
-  .then(res => res.json())
-  .then(events => {
-    const eventElement = document.getElementById('event')
-    const eventElements = events.map(createNewEvent).join('')
-    eventElement.innerHTML = eventElements
-  })
+// fetch(`http://localhost:3248/events/category/${category}`)
+//   .then(res => res.json())
+//   .then(events => {
+//     const eventElement = document.getElementById('event')
+//     const eventElements = events.map(createNewEvent).join('')
+//     eventElement.innerHTML = eventElements
+//   })
 
 const headerEl = document.getElementById('header')
 headerEl.innerHTML = createHeader({})
+const sideEl = document.getElementById('sidebar')
+console.log(sideEl, createSidebar({}))
+sideEl.innerHTML = createSidebar({})
