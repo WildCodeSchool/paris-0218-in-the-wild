67a19243a91ef3ed2960e496fa13dbf6b6d38cbb
Christopher Fremond <christopher.fremond@gmail.com>
2018-04-23 11:43:52 +0200

    stast to change branch

--- client/js/components/event.js
+++ client/js/components/event.js
@@ -1,6 +1,6 @@
 export const createNewEvent = (event) =>
   `<div>
-  <a href="event.html"><div class= "column event">
+  <a href="event.html?id=${event.id}"><div class= "column event">
   <p class="date">${event.startingTime}</p>
   <h2 class="title">${event.title}</h2>
   </div>
